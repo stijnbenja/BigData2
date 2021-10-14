43ba642256d7c21e8bb895868d4555ca7e3f13e4
Stijn <stijn@forcai.eu>
2021-10-14 20:30:19 +0200

    cleaner & debug

--- functions.py
+++ functions.py
@@ -1,20 +1,6 @@
 import numpy as np
 import pandas as pd
 
-def create_base(weeks=range(90), customers=range(2000)):
-    
-    products = range(250)
-
-    n_weeks, n_customers, n_products = len(weeks), len(customers), len(products)
-
-    base = pd.DataFrame({
-    'week': np.array([[x] * n_products * n_customers for x in weeks]).flatten(),
-    'customer': np.array([[x] * n_products for x in customers] * n_weeks).flatten(),
-    'product': list(range(n_products)) * n_customers * n_weeks
-    })
-    
-    return base
-
 def get_baskets():   
     datasets_path = lambda  file_name: f'/Users/stijnvanleeuwen/Desktop/codes/EUR/Ass2/datasets/{file_name}.parquet'
     return pd.read_parquet(datasets_path('baskets')).astype({'week':'uint8', 'customer':'uint','product':'category', 'price':'uint16'}) 
